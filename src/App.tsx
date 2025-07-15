7561e8d3b8ecdabe01618eae2529279bff187001
Patrick van der Bijl <patrick@sendt.nl>
2025-07-15 14:49:59 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import "./App.css";
-import { TypographyH1 } from "./components/ui/typography-h1";
 
 function App() {
     return (
         <>
-            <TypographyH1>This is some dummy text.</TypographyH1>
+            <h1>This is some dummy text.</h1>
         </>
     );
 }
